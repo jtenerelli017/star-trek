32718b036e1346ba0d730cae1e6dc1da2444ac6b
Justin Tenerelli <jtenerelli1@gmail.com>
2023-07-29 19:30:57 -0400

    takes req from frontend and sends to db

--- server/index.js
+++ server/index.js
@@ -1,18 +1,38 @@
-const express = require('express')
-const app = express()
-const mysql = require('mysql')
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: 'Illuminatiuh3h36',
-    database: "star_trek"
+  user: "root",
+  host: "localhost",
+  password: "password",
+  database: "star_trek",
 });
 
-app.post('/create', (req, res) => {
-    
-})
+app.post("/create", (req, res) => {
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+  const gender = req.body.gender;
+  const species = req.body.species;
+  const affiliation = req.body.affiliation;
 
-app.listen(3001, ()=> {
-    console.log("Server running on port 3001");
-})
+  db.query(
+    "INSERT INTO personnel (first_name, last_name, gender, species, affiliation) VALUES (?, ?, ?, ?, ?)",
+    [first_name, last_name, gender, species, affiliation],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values inserted");
+      }
+    }
+  );
+});
+
+app.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
