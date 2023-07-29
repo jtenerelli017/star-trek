73e8bd7b6b3fb93cdf63e32bc8c6ed218d015c59
Justin Tenerelli <jtenerelli1@gmail.com>
2023-07-29 18:12:10 -0400

    added input forms that display info to console

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,53 @@
+import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const [firstName, setFirstName] = useState(null)
+  const [lastName, setLastName] = useState(null)
+  const [gender, setGender] = useState(null)
+  const [species, setSpecies] = useState(null)
+  const [affiliation, setAffiliation] = useState(null)
+
+  const addPersonnel = () => {
+    console.log(firstName)
+    console.log(lastName)
+    console.log(gender)
+    console.log(species)
+    console.log(affiliation)
+  };
+
   return (
-    <div className="App">Hello world!</div>
+    <div>
+      <Form.Label>First Name</Form.Label>
+      <Form.Control
+        onChange={(event) => {
+          setFirstName(event.target.value)
+        }}/>
+      <Form.Label>Last Name</Form.Label>
+      <Form.Control
+        onChange={(event) => {
+          setLastName(event.target.value)
+        }}/>
+      <Form.Label>Gender</Form.Label>
+      <Form.Control
+        onChange={(event) => {
+          setGender(event.target.value)
+        }}/>
+      <Form.Label>Species</Form.Label>
+      <Form.Control
+        onChange={(event) => {
+          setSpecies(event.target.value)
+        }}/>
+      <Form.Label>Affiliation</Form.Label>
+      <Form.Control
+        onChange={(event) => {
+          setAffiliation(event.target.value)
+        }}/>
+      <Button variant="primary" onClick={addPersonnel}>Add Personnel</Button>{' '}
+    </div>
   );
 }
 
