34d77e442481e9d4fd8ee8e8d067e516a3f6b4dc
Justin Tenerelli <jtenerelli1@gmail.com>
2023-07-31 14:46:15 -0400

    switch between creating and reading personnel

--- client/src/components/Personnel.js
+++ client/src/components/Personnel.js
@@ -1,15 +1,33 @@
-import Crud from "./Crud";
+import { useState } from "react";
 import PersonnelCreate from "./PersonnelCreate";
+import PersonnelRead from "./PersonnelRead";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
 
 function Personnel() {
-  return (
-    <div className="personnel-container">
-      <Crud />
-      <PersonnelCreate />
-    </div>
-  );
+  const [crud, setCrud] = useState(0);
+
+  if (crud === 0) {
+    return (
+      <div className="align">
+        <div id="crud-buttons-container">
+          <button type="radio" id="c" onClick={() => setCrud(0)}>Create</button>
+          <button type="radio" id="r" onClick={() => setCrud(1)}>Read</button>
+        </div>
+        <PersonnelCreate />
+      </div>
+    );
+  } else {
+    return (
+      <div className="align">
+        <div id="crud-buttons-container">
+          <button type="radio" id="c" onClick={() => setCrud(0)}>Create</button>
+          <button type="radio" id="r" onClick={() => setCrud(1)}>Read</button>
+        </div>
+        <PersonnelRead />
+      </div>
+    );
+  }
 }
 
 export default Personnel;
