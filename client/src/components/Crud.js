a4d40dc7e139b214c6353c361663368f534a6ffa
Justin Tenerelli <jtenerelli1@gmail.com>
2023-07-31 01:47:29 -0400

    simplified crud buttons due to conflics

--- client/src/components/Crud.js
+++ client/src/components/Crud.js
@@ -1,25 +1,12 @@
-import ToggleButton from "react-bootstrap/ToggleButton";
-import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Crud() {
   return (
-    <div>
-        <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-          <ToggleButton id="tbg-radio-1" value={1}>
-            Create
-          </ToggleButton>
-          <ToggleButton id="tbg-radio-2" value={2}>
-            Read
-          </ToggleButton>
-          <ToggleButton id="tbg-radio-3" value={3}>
-            Update
-          </ToggleButton>
-          <ToggleButton id="tbg-radio-4" value={4}>
-            Destroy
-          </ToggleButton>
-        </ToggleButtonGroup>
-      </div>
+    <div className="crud">
+      <button type="radio" id="c">Create</button>
+      <button type="radio" id="r">Read</button>
+    </div>
   );
 }
 
