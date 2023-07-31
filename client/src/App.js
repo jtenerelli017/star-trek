0d73ccc36716df20b2f763649d3c4ce248009f4e
Justin Tenerelli <jtenerelli1@gmail.com>
2023-07-31 01:47:19 -0400

    broke down components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Personnel from "./components/Personnel";
+import TableSelect from "./components/TableSelect";
 import "./App.css";
 import star_trek_jpg from "./star-trek.jpg"
 
@@ -9,7 +10,8 @@ function App() {
       <img src={star_trek_jpg} alt="Star Trek background" id="star-trek-jpg" />
       <div id="app">
         <p id="title"><i><b>Star Trek Database</b></i></p>
-        <Personnel/ >
+        <TableSelect />
+        <Personnel />
       </div>
     </div>
   );
