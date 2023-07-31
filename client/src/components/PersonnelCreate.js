0e0dc4414114b4daba06319cbbd6db50634dff99
Justin Tenerelli <jtenerelli1@gmail.com>
2023-07-31 01:47:50 -0400

    removed line, caused issues if null

--- client/src/components/PersonnelCreate.js
+++ client/src/components/PersonnelCreate.js
@@ -19,7 +19,6 @@ function PersonnelCreate() {
   // 3 = user error
 
   const addPersonnel = () => {
-    setGender(gender.toUpperCase());
     Axios.post("/create", {
       first_name: firstName,
       last_name: lastName,
@@ -48,35 +47,45 @@ function PersonnelCreate() {
 
   return (
     <div className="personnel-container">
+      <p className="instructions">Create a personnel entry here. Enter a first and last name, gender, species, and affiliation.
+      The "gender" field must take one of 'm' (for male), 'f' (for female), or 'o' (for other/non-binary). Note that this field is
+      not case-sensitive, but the others are.</p>
+      <p className="instructions">Arguments cannot 1) be empty, 2) be more than 45 characters long, and/or 3) start with a SPACE.
+      Prohibited characters include: @ # $ % ^ & * ( ) [ ] { } ; : ' " / \ ,</p>
         <div className="prompts-container">
         <Form.Label className="prompt-label"><b>First Name</b></Form.Label>
         <Form.Control className="input"
           onChange={(event) => {
             setFirstName(event.target.value);
+            setStatusNum(0);
           }}
         />
         <Form.Label className="prompt-label"><b>Last Name</b></Form.Label>
         <Form.Control className="input"
           onChange={(event) => {
             setLastName(event.target.value);
+            setStatusNum(0);
           }}
         />
         <Form.Label className="prompt-label"><b>Gender</b></Form.Label>
         <Form.Control className="input"
           onChange={(event) => {
             setGender(event.target.value);
+            setStatusNum(0);
           }}
         />
         <Form.Label className="prompt-label"><b>Species</b></Form.Label>
         <Form.Control className="input"
           onChange={(event) => {
             setSpecies(event.target.value);
+            setStatusNum(0);
           }}
         />
         <Form.Label className="prompt-label"><b>Affiliation</b></Form.Label>
         <Form.Control className="input"
           onChange={(event) => {
             setAffiliation(event.target.value);
+            setStatusNum(0);
           }}
         />
       </div>
