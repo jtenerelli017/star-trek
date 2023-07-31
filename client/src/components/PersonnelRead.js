c6f9f08811055dbc907e725582bb7b732ab6b9a4
Justin Tenerelli <jtenerelli1@gmail.com>
2023-07-31 16:17:29 -0400

    clarified personnel read instructions

--- client/src/components/PersonnelRead.js
+++ client/src/components/PersonnelRead.js
@@ -3,8 +3,8 @@ import "../App.css";
 function PersonnelRead() {
   return (
     <div className="personnel-container">
-      <p className="instructions">Here you can read about personnel currently stored in the database. Select
-      an entry to expand it and reveal biographical data.</p>
+      <p className="instructions">Here you can read about personnel currently stored in the database. Wait a minute...
+      huh, I guess this feature hasn't been added yet. Maybe someday it will...</p>
     </div>
   );
 }
