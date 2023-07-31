70f2826977b89f65be848eb963e076e8b2821320
Justin Tenerelli <jtenerelli1@gmail.com>
2023-07-31 14:46:54 -0400

    dummy personnel read component

---
+++ client/src/components/PersonnelRead.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
+
+function PersonnelRead() {
+  return (
+    <div className="personnel-container">
+      <p className="instructions">Here you can read about personnel currently stored in the database. Select
+      an entry to expand it and reveal biographical data.</p>
+    </div>
+  );
+}
+
+export default PersonnelRead;
