8fe26d3e68c2a12c52370c4a527fa80a2b087e7b
Justin Tenerelli <jtenerelli1@gmail.com>
2023-07-31 20:21:34 -0400

    can now read all personnel in a table

--- client/src/components/PersonnelRead.js
+++ client/src/components/PersonnelRead.js
@@ -1,12 +1,56 @@
+import Axios from "axios";
+import StatusMessage from "./StatusMessage";
+import ListPersonnel from "./ListPersonnel";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
 
 function PersonnelRead() {
-  return (
-    <div className="personnel-container">
-      <p className="instructions">Here you can read about personnel currently stored in the database. Wait a minute...
-      huh, I guess this feature hasn't been added yet. Maybe someday it will...</p>
-    </div>
-  );
+  const [personnelList, setPersonnelList] = useState(null);
+  const [statusNum, setStatusNum] = useState(0);
+  // 0 = nothing
+  // 1 = success
+  // 2 = network error
+  // 3 = user error
+  // 4 = data exists
+
+  const getPersonnel = () => {
+    Axios.get("/readPersonnel")
+      .then((res) => {
+        setPersonnelList(res.data);
+        setStatusNum(1);
+      }).catch(err => {
+        console.log(err)
+        setStatusNum(2);
+      });
+  };
+
+  if (statusNum !== 1) {
+    return (
+      <div id="personnel-container" className="align">
+        <p className="instructions">
+          Read a list of all personnel here.
+        </p>
+        <div className="generate">
+          <button onClick={getPersonnel}>Generate List</button>
+        </div>
+        <StatusMessage statusNum={statusNum} />
+      </div>
+    );
+  } else {
+    return (
+      <div id="personnel-container" className="align">
+        <p className="instructions">
+          Read a list of all personnel here.
+        </p>
+        <div className="generate">
+          <button onClick={getPersonnel}>Generate List</button>
+        </div>
+        <ListPersonnel personnelList={personnelList}/>
+        <StatusMessage statusNum={statusNum} />
+      </div>
+    );
+  };
 }
 
 export default PersonnelRead;
