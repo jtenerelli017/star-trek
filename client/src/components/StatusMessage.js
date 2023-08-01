dc8fdfcf76b666069e957aec421dcd43bca0c05c
Justin Tenerelli <jtenerelli1@gmail.com>
2023-07-31 20:21:43 -0400

    made more general

--- client/src/components/StatusMessage.js
+++ client/src/components/StatusMessage.js
@@ -4,7 +4,7 @@ function StatusMessage(statusNum) {
   if (statusNum.statusNum === 0)
     return (<></>);
   if (statusNum.statusNum === 1)
-    return (<div><p className="status" id="s1">Successfully added data!</p></div>);
+    return (<div><p className="status" id="s1">Success!</p></div>);
   if (statusNum.statusNum === 2)
     return (<div><p className="status" id="s2">ERROR: Attempt to connect to server failed. Check your connection and try again.</p></div>);
   if (statusNum.statusNum === 3)
