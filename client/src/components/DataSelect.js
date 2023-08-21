7b73a902ae0ed584ffb12247a07f42e89550ecc8
Justin Tenerelli <jtenerelli1@gmail.com>
2023-08-21 14:29:14 -0400

    simplified with ternary op

--- client/src/components/DataSelect.js
+++ client/src/components/DataSelect.js
@@ -8,64 +8,27 @@ import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 function DataSelect() {
   const [table, setTable] = useState(0);
 
-  if (table === 0) {
-    return (
-      <div className="align">
-        <div id="table-select-radio-container">
-          <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-            <ToggleButton id="tbg-radio-1" value={1} onClick={() => setTable(0)}>
-              Personnel
-            </ToggleButton>
-            <ToggleButton id="tbg-radio-2" value={2} onClick={() => setTable(1)}>
-              Starship
-            </ToggleButton>
-            <ToggleButton id="tbg-radio-3" value={3} onClick={() => setTable(2)}>
-              Roster
-            </ToggleButton>
-          </ToggleButtonGroup>
-        </div>
-        <Personnel />
+  return (
+    <div className="align">
+      <div id="table-select-radio-container">
+        <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+          <ToggleButton id="tbg-radio-1" value={1} onClick={() => setTable(0)}>
+            Personnel
+          </ToggleButton>
+          <ToggleButton id="tbg-radio-2" value={2} onClick={() => setTable(1)}>
+            Starship
+          </ToggleButton>
+          <ToggleButton id="tbg-radio-3" value={3} onClick={() => setTable(2)}>
+            Roster
+          </ToggleButton>
+        </ToggleButtonGroup>
       </div>
-    );
-  } else if (table === 1) {
-    return (
-      <div className="align">
-        <div id="table-select-radio-container">
-          <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-            <ToggleButton id="tbg-radio-1" value={1} onClick={() => setTable(0)}>
-              Personnel
-            </ToggleButton>
-            <ToggleButton id="tbg-radio-2" value={2} onClick={() => setTable(1)}>
-              Starship
-            </ToggleButton>
-            <ToggleButton id="tbg-radio-3" value={3} onClick={() => setTable(2)}>
-              Roster
-            </ToggleButton>
-          </ToggleButtonGroup>
-        </div>
-        <StarshipCreate />
-      </div>
-    );
-  } else {
-    return (
-      <div className="align">
-        <div id="table-select-radio-container">
-          <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-            <ToggleButton id="tbg-radio-1" value={1} onClick={() => setTable(0)}>
-              Personnel
-            </ToggleButton>
-            <ToggleButton id="tbg-radio-2" value={2} onClick={() => setTable(1)}>
-              Starship
-            </ToggleButton>
-            <ToggleButton id="tbg-radio-3" value={3} onClick={() => setTable(2)}>
-              Roster
-            </ToggleButton>
-          </ToggleButtonGroup>
-        </div>
-        <RosterCreate />
-      </div>
-    );
-  }
+      {table === 0 ? <Personnel />
+        : table === 1 ? <StarshipCreate />
+        : <RosterCreate />
+      }
+    </div>
+  );
 }
 
 export default DataSelect;
