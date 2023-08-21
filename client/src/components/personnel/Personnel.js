20d5eb0de693bd068b60f6078b6d0a3eac989e16
Justin Tenerelli <jtenerelli1@gmail.com>
2023-08-21 17:07:44 -0400

    ternary ops

--- client/src/components/personnel/Personnel.js
+++ client/src/components/personnel/Personnel.js
@@ -1,31 +1,27 @@
 import { useState } from "react";
 import PersonnelCreate from "./PersonnelCreate";
+import PersonnelDestroy from "./PersonnelDestroy";
 import PersonnelRead from "./PersonnelRead";
+import PersonnelUpdate from "./PersonnelUpdate";
 
 function Personnel() {
   const [crud, setCrud] = useState(0);
 
-  if (crud === 0) {
-    return (
-      <div className="align">
-        <div id="crud-buttons-container">
-          <button type="radio" id="c" onClick={() => setCrud(0)}>Create</button>
-          <button type="radio" id="r" onClick={() => setCrud(1)}>Read</button>
-        </div>
-        <PersonnelCreate />
+  return (
+    <div className="align">
+      <div id="crud-buttons-container">
+        <button type="radio" onClick={() => setCrud(0)}>Create</button>
+        <button type="radio" onClick={() => setCrud(1)}>Read</button>
+        <button type="radio" onClick={() => setCrud(2)}>Update</button>
+        <button type="radio" onClick={() => setCrud(3)}>Destroy</button>
       </div>
-    );
-  } else {
-    return (
-      <div className="align">
-        <div id="crud-buttons-container">
-          <button type="radio" id="c" onClick={() => setCrud(0)}>Create</button>
-          <button type="radio" id="r" onClick={() => setCrud(1)}>Read</button>
-        </div>
-        <PersonnelRead />
-      </div>
-    );
-  }
+      {crud === 0 ? <div><p className="header"><b>Create Personnel Entry</b></p><PersonnelCreate /></div>
+        : crud === 1 ? <div><p className="header"><b>Read Personnel Entry</b></p><PersonnelRead /></div>
+        : crud === 2 ? <div><p className="header"><b>Update Personnel Entry</b></p><PersonnelUpdate /></div>
+        : <div><p className="header"><b>Destroy Personnel Entry</b></p><PersonnelDestroy /></div>
+      }
+    </div>
+  );
 }
 
 export default Personnel;
