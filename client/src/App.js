71651f683d67f7befb3a3746470b01a48fce502e
Justin Tenerelli <jtenerelli1@gmail.com>
2023-08-28 19:58:09 -0400

    new vignette adds transition to star trek pic

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import DataSelect from "./components/DataSelect";
 import React from "react";
 import star_trek_jpg from "./star-trek.jpg"
+import vignette_bottom from "./vignette-bottom.png";
 
 function App() {
   return (
     <div>
-      <img src={star_trek_jpg} alt="Star Trek background" id="star-trek-jpg" />
+      <img src={star_trek_jpg} alt="Star Trek background" id="star-trek-jpg"/>
+      <img src={vignette_bottom} alt="Vignette bottom" id="vignette"/>
       <div id="app-container">
         <p id="title"><i><b>Star Trek Database</b></i></p>
         <DataSelect />
