08865d36823c6f798bd1a8be998fd75da77e0710
Justin Tenerelli <jtenerelli1@gmail.com>
2023-08-31 17:23:37 -0400

    added more status messages & style now in element

--- client/src/components/StatusMessage.js
+++ client/src/components/StatusMessage.js
@@ -1,10 +1,13 @@
 function StatusMessage(statusNum) {
   const num = statusNum.statusNum
   return num === 0 ? (<></>)
-    : num === 1 ? (<div><p className="status" id="s1">Success!</p></div>)
-    : num === 2 ? (<div><p className="status" id="s2">ERROR: Attempt to connect to server failed. Check your connection and try again.</p></div>)
-    : num === 3 ? (<div><p className="status" id="s3">ERROR: Invalid argument(s). Read the instructions above and try again.</p></div>)
-    : (<div><p className="status" id="s3">ERROR: Data already exists. Check your arguments and try again.</p></div>)
+    : num === 1 ? (<div><p className="status" style={{color: "lightgreen"}}>Success!</p></div>)
+    : num === 2 ? (<div><p className="status" style={{color: "lightpink"}}>ERROR: Attempt to connect to server failed. Check your connection and try again.</p></div>)
+    : num === 3 ? (<div><p className="status" style={{color: "lightcoral"}}>ERROR: Invalid argument(s). Read the instructions above and try again.</p></div>)
+    : num === 4 ? (<div><p className="status" style={{color: "lightcyan"}}>No personnel entry exists in the database.</p></div>)
+    : num === 5 ? (<div><p className="status" style={{color: "coral"}}>ERROR: Personnel entry no longer exists in the database.</p></div>)
+    : num === 6 ? (<div><p className="status" style={{color: "white"}}>Loading...</p></div>)
+    : (<div><p className="status" style={{color: "red"}}>ERROR: An unknown error has occurred.</p></div>)
 }
 
 export default StatusMessage;
