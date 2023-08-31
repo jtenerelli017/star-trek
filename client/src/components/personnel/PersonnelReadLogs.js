7bee66a20369e0f92f0b9890ef2946aa747f6d00
Justin Tenerelli <jtenerelli1@gmail.com>
2023-08-31 17:24:16 -0400

    displays table if captain and no table if not

--- client/src/components/personnel/PersonnelReadLogs.js
+++ client/src/components/personnel/PersonnelReadLogs.js
@@ -1,28 +1,29 @@
 function PersonnelReadLogs(props) {
     const logs = props.personnelLogs;
-    return (
-      <div className="personnel-read-table">
-        <table className="table">
-          <caption className="table-caption">Captain's Logs: {props.name}</caption>
-          <tbody>
-            <tr>
-              <th>Captain ID</th>
-              <th>Star Date</th>
-              <th>Message</th>
-            </tr>
-            {logs.map((val, key) => {
-              return (
-                <tr key={key}>
-                  <td>{val.captain_id}</td>
-                  <td>{val.star_date}</td>
-                  <td>{val.message}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    );
+    const isCaptain = props.isCaptain;
+    
+    return !isCaptain ? <></>
+      : <div className="personnel-read-table">
+          <table className="table">
+            <caption className="table-caption">Captain's Logs: {props.name}</caption>
+            <tbody>
+              <tr>
+                <th>Captain ID</th>
+                <th>Star Date</th>
+                <th>Message</th>
+              </tr>
+              {logs.map((val, key) => {
+                return (
+                  <tr key={key}>
+                    <td>{val.captain_id}</td>
+                    <td>{val.star_date}</td>
+                    <td>{val.message}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
   }
   
   export default PersonnelReadLogs;
