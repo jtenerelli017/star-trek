c02fa1729f2ef707f1b18dfe0839ef6865d43cc8
Justin Tenerelli <jtenerelli1@gmail.com>
2023-09-01 00:16:38 -0400

    prettier...?

--- client/src/components/StatusMessage.js
+++ client/src/components/StatusMessage.js
@@ -1,13 +1,51 @@
-function StatusMessage(statusNum) {
-  const num = statusNum.statusNum
-  return num === 0 ? (<></>)
-    : num === 1 ? (<div><p className="status" style={{color: "lightgreen"}}>Success!</p></div>)
-    : num === 2 ? (<div><p className="status" style={{color: "lightpink"}}>ERROR: Attempt to connect to server failed. Check your connection and try again.</p></div>)
-    : num === 3 ? (<div><p className="status" style={{color: "lightcoral"}}>ERROR: Invalid argument(s). Read the instructions above and try again.</p></div>)
-    : num === 4 ? (<div><p className="status" style={{color: "lightcyan"}}>No personnel entry exists in the database.</p></div>)
-    : num === 5 ? (<div><p className="status" style={{color: "coral"}}>ERROR: Personnel entry no longer exists in the database.</p></div>)
-    : num === 6 ? (<div><p className="status" style={{color: "white"}}>Loading...</p></div>)
-    : (<div><p className="status" style={{color: "red"}}>ERROR: An unknown error has occurred.</p></div>)
+function StatusMessage(props) {
+  const num = props.statusNum;
+  return num === 0 ? (
+    <></>
+  ) : num === 1 ? (
+    <div>
+      <p className="status" style={{ color: "lightgreen" }}>
+        Success!
+      </p>
+    </div>
+  ) : num === 2 ? (
+    <div>
+      <p className="status" style={{ color: "lightpink" }}>
+        ERROR: Attempt to connect to server failed. Check your connection and
+        try again.
+      </p>
+    </div>
+  ) : num === 3 ? (
+    <div>
+      <p className="status" style={{ color: "lightcoral" }}>
+        ERROR: Invalid argument(s). Read the instructions above and try again.
+      </p>
+    </div>
+  ) : num === 4 ? (
+    <div>
+      <p className="status" style={{ color: "lightcyan" }}>
+        No personnel entry exists in the database.
+      </p>
+    </div>
+  ) : num === 5 ? (
+    <div>
+      <p className="status" style={{ color: "coral" }}>
+        ERROR: Personnel entry no longer exists in the database.
+      </p>
+    </div>
+  ) : num === 6 ? (
+    <div>
+      <p className="status" style={{ color: "white" }}>
+        Loading...
+      </p>
+    </div>
+  ) : (
+    <div>
+      <p className="status" style={{ color: "red" }}>
+        ERROR: An unknown error has occurred.
+      </p>
+    </div>
+  );
 }
 
 export default StatusMessage;
